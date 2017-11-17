d06803d1e410fe8784932cc1df647d192917525f
reynold-james <32459283+reynold-james@users.noreply.github.com>
2017-11-17 15:34:55 -0500

    upload1

--- world.php
+++ world.php
@@ -1,18 +1,30 @@
 <?php
-
 $host = getenv('IP');
 $username = getenv('C9_USER');
 $password = '';
 $dbname = 'world';
-
+$country= $_GET['country'];//Get country to search for
+$all=$_GET['all'];
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-
-$stmt = $conn->query("SELECT * FROM countries");
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo '<ul>';
-foreach ($results as $row) {
-  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+if(isset($all) == true && isset($country) == true)
+{
+    if($all == 'true')
+    {
+        $stmt = $conn->query('SELECT * FROM countries');
+    }
+    else
+    {
+        $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    }
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    echo '<ul>';
+    foreach ($results as $row) {
+      echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+    echo '</ul>';
 }
-echo '</ul>';
+else
+{
+    echo ' ';
+}
